e754e3286426082718779bfb69bf7472820b8ab0
Clayton Hynfield <clayton@hynfield.org>
2018-06-24 03:56:15 +0000

    Refactor get-dependencies.sh
    - Make Vundle installation idempotent
    - Only use sudo if not already effectively root

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 if [ -r '/etc/os-release' ]; then
     source '/etc/os-release'
 fi
 
+function with_privelege() {
+    if [[ $EUID -ne 0 ]]; then
+        sudo "$@"
+    else
+        "$@"
+    fi
+}
+
 if [ "$ID" == 'debian' -o "$ID_LIKE" == 'debian' ]; then
-    sudo apt-get install -y automake build-essential pkg-config libevent-dev libncurses5-dev
+    with_privelege apt-get update
+    with_privelege apt-get install -y automake build-essential pkg-config libevent-dev libncurses5-dev
 fi
