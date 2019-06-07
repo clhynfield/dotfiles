b41c7feb009df6b8c67a2506ae18d3e393788859
Clayton Hynfield <clayton@hynfield.org>
2019-06-07 11:33:50 -0400

    Correct typo

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,7 +4,7 @@ if [ -r '/etc/os-release' ]; then
     source '/etc/os-release'
 fi
 
-function with_privelege() {
+function with_privilege() {
     if [[ $EUID -ne 0 ]]; then
         sudo "$@"
     else
@@ -13,8 +13,8 @@ function with_privelege() {
 }
 
 if [ "$ID" == 'debian' -o "$ID_LIKE" == 'debian' ]; then
-    with_privelege apt-get update
-    with_privelege apt-get install -y sudo automake build-essential pkg-config libevent-dev libncurses5-dev
+    with_privilege apt-get update
+    with_privilege apt-get install -y sudo automake build-essential pkg-config libevent-dev libncurses5-dev
 fi
 
 if [ $OSTYPE == 'darwin' ]; then
@@ -26,7 +26,7 @@ fi
 if [ $OSTYPE == 'darwin' ]; then
     brew install direnv
 elif [ "$ID" == 'debian' -o "$ID_LIKE" == 'debian' ]; then
-    with_privelege apt-get install -y direnv
+    with_privilege apt-get install -y direnv
 fi
 
 
