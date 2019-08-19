0fc0d6e227e7847e76198cd17f8c5d905b83bc6c
Clayton Hynfield <clayton@hynfield.org>
2019-08-19 10:41:28 -0400

    Refactor ambiguous conditionals in get-dependencies
    Thanks, [shellcheck](https://www.shellcheck.net)!

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -r '/etc/os-release' ]; then
     source '/etc/os-release'
@@ -12,20 +12,20 @@ function with_privilege() {
     fi
 }
 
-if [ "$ID" == 'debian' -o "$ID_LIKE" == 'debian' ]; then
+if [ "$ID" == 'debian' ] || [ "$ID_LIKE" == 'debian' ]; then
     with_privilege apt-get update
     with_privilege apt-get install -y sudo automake build-essential pkg-config libevent-dev libncurses5-dev
 fi
 
-if [ $OSTYPE == 'darwin' ]; then
+if [ "$OSTYPE" == 'darwin' ]; then
     brew install getantibody/tap/antibody
 else
     curl -sL git.io/antibody | sh -s
 fi
 
-if [ $OSTYPE == 'darwin' ]; then
+if [ "$OSTYPE" == 'darwin' ]; then
     brew install direnv
-elif [ "$ID" == 'debian' -o "$ID_LIKE" == 'debian' ]; then
+elif [ "$ID" == 'debian' ] || [ "$ID_LIKE" == 'debian' ]; then
     with_privilege apt-get install -y direnv
 fi
 
